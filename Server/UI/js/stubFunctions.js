d16c00aa7f4893e024e360ca852d173bfd8abd02
Rob Emery <rob@mintsoft.net>
2012-05-06 20:17:23 +0100

    Implementing genericRetrieve_preferencesObj with sensible defaults

--- Server/UI/js/stubFunctions.js
+++ Server/UI/js/stubFunctions.js
@@ -6,9 +6,14 @@ function genericPostMessage(objToSend){
 
 //stub to access opera.extension.bgProcess.mintSyncGlobals
 function genericRetrieve_mintSyncGlobals(){
-
+	return {};
 }
 
+//temporary stub to hardcode sensible defaults for server-side interface
 function genericRetrieve_preferencesObj(){
-
+	return { 
+		'ServerURL' : '../',
+		'SavePassword' : '0.5',
+		'SaveAuthentication' : '0.5'
+	};
 }
