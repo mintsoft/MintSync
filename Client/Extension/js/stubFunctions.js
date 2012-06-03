148c23cb608d837d849b966c80e1bba093de83ae
Rob Emery <rob@mintsoft.net>
2012-06-03 21:58:33 +0100

    Branch TidyUp Importing example manifest and chromium specifics for popups

--- Client/Extension/js/stubFunctions.js
+++ Client/Extension/js/stubFunctions.js
@@ -10,5 +10,8 @@ function genericRetrieve_mintSyncGlobals(){
 }
 
 function genericRetrieve_preferencesObj(){
+	if( typeof localStorage["MintSync_preferencesObject"] === "undefined" )
+		localStorage["MintSync_preferencesObject"] = {}
+	return localStorage["MintSync_preferencesObject"];
 
 }
