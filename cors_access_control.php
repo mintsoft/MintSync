5e9c979022117e295ae8a9361b9042c3341e0c1b
Rob Emery <rob@mintsoft.net>
2013-07-14 20:20:24 +0100

    Fixing CORS headers (authorisation not authentication you pillock!)

--- cors_access_control.php
+++ cors_access_control.php
@@ -5,9 +5,9 @@ if(strtolower($_SERVER['REQUEST_METHOD']) == "options")
 	//header("Access-Control-Allow-Origin: ".$_SERVER['HTTP_ORIGIN']);
 	header("Access-Control-Allow-Origin: *");
 	header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-	header("Access-Control-Allow-Headers: X-MS-Authentication");
+	header("Access-Control-Allow-Headers: X-MS-Authorisation, Content-Type");
 	header("Access-Control-Max-Age: 3600");
-	header("Content-Type: text/plain");
+	//header("Content-Type: text/plain");
 	die;
 }
 ?>
