78f025c9f41ac250a53b327df143da6d03da10be
Rob Emery <rob@mintsoft.net>
2013-09-07 22:37:50 +0100

    Adding config key for migrations directory

--- config.php
+++ config.php
@@ -4,6 +4,7 @@ define("PASSWORD_DATABASE", "/var/wwws/code/MintSync/ServerUI/db/pass.db");
 define("PDO_DSN", 'sqlite:' . PASSWORD_DATABASE);
 define("LOGGING", true);
 define("SERVER_UI_LOCKED", false);
+define("MIGRATIONS_DIRECTORY","./migrations");
 
 $LOGLEVEL = array(
 	"retrieve" => true,
