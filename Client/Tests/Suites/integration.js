3fc7e71816b3aed897852fd80e35c108c282ae07
Rob Emery <rob@mintsoft.net>
2014-10-18 21:44:22 +0100

    Integration Test: can retrieve the credentials blob for an URL

--- Client/Tests/Suites/integration.js
+++ Client/Tests/Suites/integration.js
@@ -1,7 +1,7 @@
 QUnit.asyncTest("The Server API can return a list of URLS correctly", function (assert) {
 	$MS.listURLS({
 		error: function(jq,textStatus,errorThrown) {
-			assert.equal(1,0,"list URLS has failed: this has failed: " + errorThrown)
+			assert.equal(1,0,"list URLS has failed: " + errorThrown)
 			QUnit.start();
 		},
 		success: function(data){
@@ -12,8 +12,8 @@ QUnit.asyncTest("The Server API can return a list of URLS correctly", function (
 		}
 	});
 });
-/*
-QUnit.asyncTest("The client can decrypt a URL that exists", function (assert) {
+
+QUnit.asyncTest("The client can retrieve the credentials object for an URL that exists", function (assert) {
 	$MS.listURLS({
 		error: function(jq,textStatus,errorThrown) {
 			assert.equal(1,0,"list URLS has failed: this has failed: " + errorThrown)
@@ -24,18 +24,50 @@ QUnit.asyncTest("The client can decrypt a URL that exists", function (assert) {
 			assert.equal(data.action, "list");
 			var testUrl = data.data[0].URL;
 
-
-
-
-
-
-
-
-
-
-
-			QUnit.start();
+			$MS.getPasswords(testUrl,{
+				error: function(jq,textStatus,errorThrown) {
+					assert.equal(1,0,"getPasswords has failed:" + errorThrown)
+					QUnit.start();
+				},
+				success: function(response){
+					assert.equal(response.status, "ok");
+					assert.equal(response.action, "retrieve");
+					assert.ok(response.data.Salt);
+					assert.ok(response.data.Credentials);
+					QUnit.start();
+				}
+			});
 		}
 	});
 });
-*/
+
+QUnit.asyncTest("The client can decrypt the credentials object for an URL", function (assert) {
+	$MS.listURLS({
+		error: function(jq,textStatus,errorThrown) {
+			assert.equal(1,0,"list URLS has failed: this has failed: " + errorThrown);
+			QUnit.start();
+		},
+		success: function(data){
+			assert.equal(data.status, "ok");
+			assert.equal(data.action, "list");
+			var testUrl = data.data[0].URL;
+
+			$MS.getPasswords(testUrl,{
+				error: function(jq,textStatus,errorThrown) {
+					assert.equal(1,0,"getPasswords has failed:" + errorThrown);
+					QUnit.start();
+				},
+				success: function(response){
+					assert.equal(response.status, "ok");
+					assert.equal(response.action, "retrieve");
+					assert.ok(response.data.Salt);
+					assert.ok(response.data.Credentials);
+					assert.equal(0,response.data.cryptoScheme);
+
+					
+					QUnit.start();
+				}
+			});
+		}
+	});
+});
