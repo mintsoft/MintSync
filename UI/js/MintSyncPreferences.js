b2ee3e6678be421601d6041e1c47ad415573cf92
Rob <rob@mintsoft.net>
2016-08-21 22:25:36 +0100

    Fixing git merge mangling the commits

--- UI/js/MintSyncPreferences.js
+++ UI/js/MintSyncPreferences.js
@@ -3,12 +3,6 @@ function MintSyncPreferences(){
 	var preferences;
 	this.get = function(preferenceName) {
 		preferences = stubFunctions.genericRetrieve_preferencesObj();
-		if(preferenceName != "SaveAuthentication")
-		{
-			$MS.retrieveServerSavedPreferences({success: function(data) { 
-				preferences = $.extend({}, preferences, data.preferences);
-			}})
-		}
 		return preferences[preferenceName];
 	};
 	this.set = function(name, value) {
