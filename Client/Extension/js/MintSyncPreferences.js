0fd29b36ef8901f568cb4037507a4097f99fd26d
Rob <rob@mintsoft.net>
2016-08-21 22:19:34 +0100

    Preferences: Will now async load them in from the serverside when requested

--- Client/Extension/js/MintSyncPreferences.js
+++ Client/Extension/js/MintSyncPreferences.js
@@ -3,10 +3,18 @@ function MintSyncPreferences(){
 	var preferences;
 	this.get = function(preferenceName) {
 		preferences = stubFunctions.genericRetrieve_preferencesObj();
+		if(preferenceName != "SaveAuthentication")
+		{
+			$MS.retrieveServerSavedPreferences({success: function(data) { 
+				preferences = $.extend({}, preferences, data.preferences);
+			}})
+		}
 		return preferences[preferenceName];
 	};
 	this.set = function(name, value) {
 		preferences[name] = value;
+
+		$ms.UpdateServerSavedPreferences(preferences);
 	};
 }
 var $MP = new MintSyncPreferences();
