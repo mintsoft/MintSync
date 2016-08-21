3925e0c02e48c11aca5680d1e364c9ef9ab1172b
Rob <rob@mintsoft.net>
2016-08-21 21:50:58 +0100

    Moving MintSyncPreferences into its own file; so options can instantiate it without dragging in $MS

--- Client/Extension/js/MintSync.js
+++ Client/Extension/js/MintSync.js
@@ -895,15 +895,4 @@ function MintSync(userPreferenceServiceProvider) {
 	};
 }
 
-function MintSyncPreferences(){
-	var preferences;
-	this.get = function(preferenceName) {
-		preferences = stubFunctions.genericRetrieve_preferencesObj();
-		return preferences[preferenceName];
-	};
-	this.set = function(name, value) {
-		preferences[name] = value;
-	};
-}
-var $MP = new MintSyncPreferences();
 var $MS = new MintSync($MP);
