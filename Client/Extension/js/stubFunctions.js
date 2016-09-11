f2bd4c936fab4e1bda31f8aa02bd0a742616e923
Rob <rob@mintsoft.net>
2016-09-11 17:31:18 +0100

    Firefox: Some form of the preferences are persisting now

--- Client/Extension/js/stubFunctions.js
+++ Client/Extension/js/stubFunctions.js
@@ -1,23 +1,23 @@
 function MS_StubFunctions(){
-//function stub for opera.extension.postMessage
-this.genericPostMessage = function(objToSend){
+	//function stub for opera.extension.postMessage
+	this.genericPostMessage = function(objToSend){
+		chrome.runtime.sendMessage(objToSend);
+	}
 	
-}
-
-//stub to access opera.extension.bgProcess.mintSyncGlobals
-this.genericRetrieve_mintSyncGlobals = function(){
-
-}
-
-this.genericRetrieve_preferencesObj = function(){
-	return {};
-}
-
-this.genericRetrieve_currentTab = function(callback){
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		callback(tabs[0]);
-	});
-}
+	//stub to access opera.extension.bgProcess.mintSyncGlobals
+	this.genericRetrieve_mintSyncGlobals = function(){
+	return chrome.extension.getBackgroundPage().mintSyncGlobals;
+	}
+	
+	this.genericRetrieve_preferencesObj = function(){
+		return localStorage;
+	}
+	
+	this.genericRetrieve_currentTab = function(callback){
+		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+			callback(tabs[0]);
+		});
+	}
 }
 
 var stubFunctions = new MS_StubFunctions();
