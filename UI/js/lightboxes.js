673ccc2721e74a7cfb6bc8d4ba81a5f4439817e4
Rob <rob@mintsoft.net>
2017-04-17 23:41:11 +0100

    git merge doesn't work ~_~; manually updating instead

--- UI/js/lightboxes.js
+++ UI/js/lightboxes.js
@@ -287,6 +287,13 @@ function MS_Lightboxes() {
 		if(alreadyAutoSelected === 0)
 			$("#IC_ID").change();
 	}
+	this.qrModal = function()
+	{
+		$("body").append("<div id='QRModal'><div id='QRRenderTarget'></div></div>");
+		
+		initModal("#QRModal");
+	}
+	
 	this.modalThis = function(modalTarget, callbacks)
 	{
 		if (! modalTarget instanceof jQuery) {
